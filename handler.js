6c95daceefd2084809feed25422addbb7cf83c75
Edgar Hernandez Campos <96439635+edgarhdzcamposgbm@users.noreply.github.com>
2022-07-23 12:52:26 -0500

    Add serverless

---
+++ handler.js
@@ -0,0 +1,45 @@
+'use strict';
+const querystring = require("querystring")
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hola Serverless!!!!!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.helloUser = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Hola Usuario',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.createUser = async (event) => {
+  const body = querystring.parse(event["body"])
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Petición para crear usuario',
+        input: `Hola ${body.user}`,
+      },
+      null,
+      2
+    ),
+  };
+};
